eeede1d5831e588b7f8dbecff2a056e601b511c2
LaneLR <laneleerichardson@gmail.com>
2025-05-12 11:19:37 -0500

    extension 1

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-require('dotenv').config();
-const giphy = 'https://api.giphy.com/v1/gifs/search?api_key=gfMs7vGi9QVQkckQYuOcXeOqMxU3hFbv&q=spongebob&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
+require("dotenv").config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+async function getImage(query) {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=gfMs7vGi9QVQkckQYuOcXeOqMxU3hFbv&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    );
+    const data = await response.json();
+
+    const randomNum = Math.floor(Math.random() * 26); //pick random number between 0 and 25
+
+    if (data.data[randomNum]) { 
+      //if the object at index randomNum exists
+      const url = data.data[randomNum].images.original.url;
+      console.log(url);
+      return url;
+    } else {
+      console.warn("This data does not exist.");
+      return null;
+    }
+  } catch (err) {
+    console.error("Ran into an error: ", err);
+  }
+}
+
+getImage("spongebob");
