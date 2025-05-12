a5821aceb4a1e865559f131d9ce1140feea2a636
LaneLR <laneleerichardson@gmail.com>
2025-05-12 09:55:33 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const giphy = 'https://api.giphy.com/v1/gifs/search?api_key=gfMs7vGi9QVQkckQYuOcXeOqMxU3hFbv&q=spongebob&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips'
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
